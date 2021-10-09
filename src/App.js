1cfd4e4626e192004717f890544358493d008b68
avishekataur <avishekataur@gmail.com>
2021-10-09 21:15:08 +0600

    facebook-done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   GithubAuthProvider,
+  FacebookAuthProvider,
 } from "firebase/auth";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 import { useState } from "react";
@@ -14,10 +15,26 @@ initializeAuthentication();
 
 const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 function App() {
   const [user, setUser] = useState({});
   const auth = getAuth();
+
+  const facebookHandler = () => {
+    signInWithPopup(auth, facebookProvider).then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        image: photoURL,
+      };
+      setUser(loggedInUser);
+      // The signed-in user's info.
+      const user = result.user;
+      console.log(user);
+    });
+  };
 
   const googleHandler = () => {
     signInWithPopup(auth, googleProvider).then((result) => {
@@ -60,7 +77,11 @@ function App() {
     <div className="App">
       {!user.name ? (
         <div className="m-5 p-5">
-          <Button variant="outline-primary" onClick={googleHandler}>
+          <Button variant="outline-primary" onClick={facebookHandler}>
+            Sign In With Facebook
+          </Button>{" "}
+          &nbsp; &nbsp; &nbsp;
+          <Button variant="outline-danger" onClick={googleHandler}>
             Sign In With Google
           </Button>{" "}
           &nbsp; &nbsp; &nbsp;
